a5c5cddfc683eaa8e0490a7a7df6840b5829bce5
MARWANANO32 <marwanano32@gmail.com>
2024-09-29 15:14:56 +0300

    MARWAN MOSTAFA

--- utils/redis.js
+++ utils/redis.js
@@ -1,34 +1,30 @@
 const redis = require('redis');
 const { promisify } = require('util');
 
-/**
- * create the Redis Client
- */
-
-
 class RedisClient {
-    constructor() {
-        this.client = redis.createclient();
-        this.getAsync = promisify(this.client.get).bind(this.client);
-        this.client.on('error', (error) => {
-            console.log(`Redis client not connected to the server: ${error.message}`);
-        });
-    }
+  constructor() {
+    this.client = redis.createClient();
+    this.getAsync = promisify(this.client.get).bind(this.client);
+    this.client.on('error', (error) => {
+      console.log(`Redis client not connected to the server: ${error.message}`);
+    });
+  }
 
-    isAlive() {
-        return this.client.connected;
-    }
+  isAlive() {
+    return this.client.connected;
+  }
 
-    async get(key){
-        return this.getAsync(key);
-    }
+  async get(key) {
+    return this.getAsync(key);
+  }
 
-    async set(key, value, duration){
-        this.client.setex(key, value, duration);
-    }
-    async del(key){
-        this.client.del(key);
-    }
+  async set(key, value, duration) {
+    this.client.setex(key, duration, value);
+  }
+
+  async del(key) {
+    this.client.del(key);
+  }
 }
 
 const redisClient = new RedisClient();
